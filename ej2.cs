aa2550be5bf29814c873af8a121b57e1337b34fc
iFaustoo <faustoordoriguez10@gmail.com>
2025-05-30 13:19:18 +0200

    Add files via upload

---
+++ ej2.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero
+
+            Console.WriteLine("Ingrese un numero");
+            numero = int.Parse(Console.ReadLine();
+
+            if (numero == 0)
+            {
+                Console.WriteLine("Es cero");
+            }
+            else if (numero > 0)
+            {
+                Console.WriteLine("Es positivo");
+            }
+            else
+            {
+                Console.WriteLine("Es negativo");
+            }
+        }
+    }
+}
