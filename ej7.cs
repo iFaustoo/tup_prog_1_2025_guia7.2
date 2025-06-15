dc593b207824d9d4d01a4e372e41d96db195cd77
iFaustoo <faustoordoriguez10@gmail.com>
2025-06-15 20:11:52 -0300

    Add files via upload

---
+++ ej7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace año_bisiesto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int año;
+
+            Console.WriteLine("Ingrese un año");
+            año = Convert.ToInt32(Console.ReadLine());
+
+            if (año % 4 == 0)
+            {
+                if (año % 100 == 100 || año % 400 == 0)
+                {
+                    Console.WriteLine("Es un año bisiesto");
+                }
+                else
+                {
+                    Console.WriteLine("No es un año bisiesto");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No es un año bisiesto");
+            }
+        }
+    }
+}
