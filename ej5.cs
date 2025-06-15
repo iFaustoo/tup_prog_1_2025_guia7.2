f37172b54d794a3d32e060afb1ced9ad68f18e61
iFaustoo <faustoordoriguez10@gmail.com>
2025-06-15 19:37:23 -0300

    Add files via upload

---
+++ ej5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nMenor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, posicion = 0;
+            int nMenor = int.MaxValue;
+
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine("Ingrese un número");
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n < nMenor)
+                {
+                    nMenor = n;
+                    posicion = i;
+                }
+            }
+
+            Console.WriteLine($"El menor número ingresado fue {nMenor}, encontrado en la posición {posicion}");
+
+        }
+    }
+}
